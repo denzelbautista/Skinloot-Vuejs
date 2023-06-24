d19541e947a7df1ceab007e107996d4bfb5e7ed1
denzelbautista <denzel.bautista@utec.edu.pe>
2023-06-24 12:14:40 -0500

    ADD update_user

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import (Flask,request,jsonify,abort)
+from flask import (Flask, request, jsonify, abort)
 from .models import db, setup_db, Skin, User, Postventa, Transaccion, Trade
 from flask_cors import CORS
 from flask_login import login_user, login_required, current_user, LoginManager, UserMixin, logout_user
@@ -72,7 +72,7 @@ def create_app(test_config=None):
 
                 user_id = user.id
 
-                #image_dir = os.path.join("static/image/persona.jpg")
+                # image_dir = os.path.join("static/image/persona.jpg")
                 image_dir = "static/image/persona.jpg"
                 user.image = image_dir
                 db.session.commit()
@@ -82,7 +82,7 @@ def create_app(test_config=None):
             print(sys.exc_info())
             db.session.rollback()
             returned_code = 500
-            return jsonify({"success":False,"message":str(e)})
+            return jsonify({"success": False, "message": str(e)})
         finally:
             db.session.close()
 
@@ -91,8 +91,49 @@ def create_app(test_config=None):
         elif returned_code != 201:
             abort(returned_code)
         else:
-            return jsonify({'success': True, 'message': 'User created successfully!', 'user_id': user_id}), returned_code
-    
+            return jsonify({'success': True, 'message': 'User created successfully!', 'user': user.serialize()}), returned_code
+
+    @app.route('/users/<user_id>', methods=['PATCH'])
+    def update_user(user_id):
+        returned_code = 200
+        list_errors = []
+        try:
+
+            user = User.query.filter_by(id=user_id).first()
+
+            if not user:
+                returned_code = 404
+            else:
+
+                body = request.json
+
+                user.nickname = body['nickname']
+
+                balance = body.get('balance')
+
+                if balance:
+                    if user.saldo is not None:
+                        user.saldo += int(balance)
+                    else:
+                        user.saldo = int(balance)
+
+                db.session.commit()
+                user_id = user.id
+
+        except Exception as e:
+            print(sys.exc_info())
+            db.session.rollback()
+            returned_code = 500
+        finally:
+            db.session.close()
+
+        if returned_code == 400:
+            return jsonify({'success': False, 'message': 'Error updating user'}), returned_code
+        elif returned_code != 200:
+            abort(returned_code)
+        else:
+            return jsonify({'success': True, 'message': 'User updated successfully!', 'id': user_id}), returned_code
+
     @app.route('/skins', methods=['POST'])
     def register_skins():
         returned_code = 201
@@ -102,76 +143,77 @@ def create_app(test_config=None):
             if 'name' not in body:
                 list_errors.append('name is required')
             else:
-                name = body.get("name")
+                name = body.get('name')
             if 'champion_name' not in body:
                 list_errors.append('champion_name is required')
             else:
-                champion_name = body.get("champion_name")
+                champion_name = body.get('champion_name')
             if 'rarity' not in body:
                 list_errors.append('rarity is required')
             else:
                 rarity = body.get('rarity')
             if 'user_id' not in body:
-                list_errors.append("user_id is required")
+                list_errors.append('user_id is required')
             else:
                 user_id = body.get("user_id")
 
             if len(list_errors) > 0:
                 returned_code = 400
             else:
-                skin = Skin(name,champion_name,rarity,user_id)
+                skin = Skin(name, champion_name, rarity, user_id)
                 db.session.add(skin)
                 db.session.commit()
                 uid = skin.id
 
                 filename = f'{user_id}.txt'
-                filepath = os.path.join(f"{app.config['UPLOAD_FOLDER']}/{user_id}",filename)
+                filepath = os.path.join(
+                    f"{app.config['UPLOAD_FOLDER']}/{user_id}", filename)
 
-                with open(filepath,'a') as file:
+                with open(filepath, 'a') as file:
                     file.write(str(uid) + '\n')
                 file.close()
 
             db.session.commit()
         except Exception as e:
             db.session.rollback()
-            return jsonify({'success':False,'error':str(e)})
+            return jsonify({'success': False, 'error': str(e)})
         finally:
             db.session.close()
         if returned_code == 400:
-            return jsonify({'success':False,'message':"Error creating skin",'errors':list_errors}),returned_code
+            return jsonify({'success': False, 'message': "Error creating skin", 'errors': list_errors}), returned_code
         elif returned_code != 201:
             abort(returned_code)
         else:
-            return jsonify({'success':True,'message':"skin created successfully!",'skin_id':uid}),returned_code
+            return jsonify({'success': True, 'message': "skin created successfully!", 'skin_id': uid}), returned_code
 
-    @app.route('/show-skins-current/<user_id>',methods=['GET'])
+    @app.route('/show-skins-current/<user_id>', methods=['GET'])
     def current_skins(user_id):
         try:
             skins = Skin.query.filter_by(user_id=user_id).all()
             skins_serialized = [skin.serialize() for skin in skins]
-            return jsonify({'success':True,"serialize":skins_serialized})
+            return jsonify({'success': True, "serialize": skins_serialized})
         except Exception as e:
-            return jsonify({'success':False,"error":str(e)})
+            return jsonify({'success': False, "error": str(e)})
 
-    @app.route('/show-posts',methods=['GET'])
+    @app.route('/show-posts', methods=['GET'])
     def showPosts():
         try:
             posts = Postventa.query.filter_by(on_sale=True).all()
             posts_serialized = [post.serialize() for post in posts]
-            return jsonify({'success':True,'serialized':posts_serialized})
+            return jsonify({'success': True, 'serialized': posts_serialized})
         except Exception as e:
-            return jsonify({'success':False,'error':str(e)})
+            return jsonify({'success': False, 'error': str(e)})
 
-    @app.route('/show-skins',methods=['GET'])
+    @app.route('/show-skins', methods=['GET'])
     def showSkins():
         try:
             skins = Skin.query.all()
             skins_serialized = [skin.serialize() for skin in skins]
-            return jsonify({'success':True,'skins':skins_serialized})
+            return jsonify({'success': True, 'skins': skins_serialized})
         except Exception as e:
-            return jsonify({'success':False,'error':str(e)})
+            return jsonify({'success': False, 'error': str(e)})
 
-    @app.route('/post-venta/<user_id>',methods=['POST'])
+    @app.route('/post-venta/<user_id>', methods=['POST'])
     def create_postventa(user_id):
         returned_code = 201
         list_errors = []
@@ -197,27 +239,29 @@ def create_app(test_config=None):
                 list_errors.append('champion is required')
             else:
                 champion = body.get('champion')
-            
+
             if len(list_errors) > 0:
                 returned_code = 400
             else:
                 user_id = user_id
-                skin_image = os.path.join("static/campeones",f'{champion}',f'{name}.jpg')
+                skin_image = os.path.join(
+                    "static/campeones", f'{champion}', f'{name}.jpg')
                 on_sale = True
                 skin = Skin.query.get(skin_id)
                 if not skin:
-                    return jsonify({'success':False,'message':'Skin not found'})
-                postventa = Postventa(title,user_id,skin_id,on_sale,int(price),champion,skin_image,name)
+                    return jsonify({'success': False, 'message': 'Skin not found'})
+                postventa = Postventa(title, user_id, skin_id, on_sale, int(
+                    price), champion, skin_image, name)
                 db.session.add(postventa)
                 db.session.commit()
 
-                return jsonify({'success':True,'title':title,'user_id':user_id,'skin_id':skin_id,'champion':champion,'skin_image':skin_image,'on_sale':on_sale})
+                return jsonify({'success': True, 'title': title, 'user_id': user_id, 'skin_id': skin_id, 'champion': champion, 'skin_image': skin_image, 'on_sale': on_sale})
         except Exception as e:
-            return jsonify({'success':False,'message':str(e)})
+            return jsonify({'success': False, 'message': str(e)})
         finally:
             db.session.close()
 
-    @app.route('/comprar-skin/<user_id>',methods=['POST'])
+    @app.route('/comprar-skin/<user_id>', methods=['POST'])
     def comprar_skin(user_id):
         returned_code = 201
         list_errors = []
@@ -240,15 +284,15 @@ def create_app(test_config=None):
             else:
                 post_id = body.get('post_id')
 
-            if len(list_errors)>0:
+            if len(list_errors) > 0:
                 returned_code = 400
             else:
                 posteo = Postventa.query.filter_by(id=post_id).first()
                 usuario = User.query.filter_by(id=user_id).first()
                 if usuario.saldo == None or usuario.saldo == 0:
-                    return jsonify({'success':False,'message':'wallet = 0'})
+                    return jsonify({'success': False, 'message': 'wallet = 0'})
                 elif usuario.saldo < int(price):
-                    return jsonify({'success':False,'message':'insufficiente amount of money'})
+                    return jsonify({'success': False, 'message': 'insufficiente amount of money'})
                 else:
                     price = int(price)
                     comision = price * 0.05
@@ -258,26 +302,30 @@ def create_app(test_config=None):
                     seller.saldo += (price - comision)
 
                     filename_seller = f'{seller_uid}.txt'
-                    filepath_seller = os.path.join(f"{app.config['UPLOAD_FOLDER']}/{seller_uid}",filename_seller)
+                    filepath_seller = os.path.join(
+                        f"{app.config['UPLOAD_FOLDER']}/{seller_uid}", filename_seller)
 
-                    with open(filepath_seller,'r') as file:
+                    with open(filepath_seller, 'r') as file:
                         contenido = file.readlines()
 
-                    contenido = [linea for linea in contenido if linea.strip() != skin_uid]
+                    contenido = [
+                        linea for linea in contenido if linea.strip() != skin_uid]
                     file.close()
 
-                    with open(filepath_seller,'w') as file:
+                    with open(filepath_seller, 'w') as file:
                         file.writelines(contenido)
                     file.close()
 
                     filename_user = f'{user_id}.txt'
-                    filepath_user = os.path.join(f"{app.config['UPLOAD_FOLDER']}/{user_id}",filename_user)
+                    filepath_user = os.path.join(
+                        f"{app.config['UPLOAD_FOLDER']}/{user_id}", filename_user)
 
-                    with open(filepath_user,'a') as file:
+                    with open(filepath_user, 'a') as file:
                         file.write(str(skin_uid)+'\n')
                     file.close()
 
-                    boleta = Transaccion(price,comision,user_id,seller_uid,skin_uid)
+                    boleta = Transaccion(
+                        price, comision, user_id, seller_uid, skin_uid)
 
                     skin = Skin.query.filter_by(id=skin_uid).first()
                     skin.user_id = usuario.id
@@ -286,34 +334,33 @@ def create_app(test_config=None):
 
                     db.session.add(boleta)
                     db.session.commit()
-                    return jsonify({'success':True,'boleta id':boleta.id})
+                    return jsonify({'success': True, 'boleta id': boleta.id})
         except Exception as e:
-            return jsonify({'success':False,'message':str(e)})
+            return jsonify({'success': False, 'message': str(e)})
         finally:
             db.session.close()
 
     @app.route('/users/<user_id>/<user_saldo>', methods=['PATCH'])
-    def update_user_saldo(user_id,user_saldo):
+    def update_user_saldo(user_id, user_saldo):
         returned_code = 200
         list_errors = []
         try:
             user = User.query.filter_by(id=user_id).first()
-            
+
             user.saldo += int(user_saldo)
             db.session.commit()
         except Exception as e:
             db.session.rollback()
             returned_code = 500
-            return jsonify({'success':False,'message':str(e)})
+            return jsonify({'success': False, 'message': str(e)})
         finally:
             db.session.close()
         if len(list_errors) > 0:
             returned_code = 400
-            return jsonify({'success':False,'message':'Error updating employee','errors':list_errors}),returned_code
+            return jsonify({'success': False, 'message': 'Error updating employee', 'errors': list_errors}), returned_code
         elif returned_code != 200:
             abort(returned_code)
         else:
-            return jsonify({"success":True,'message':'User is updated successfully!'}),returned_code
-
+            return jsonify({"success": True, 'message': 'User is updated successfully!'}), returned_code
 
     return app
