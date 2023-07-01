2e42b242a5f06057b011f662810dadba39a82acf
denzelbautista <denzel.bautista@utec.edu.pe>
2023-06-30 20:32:33 -0500

    FIX 2

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -43,7 +43,7 @@ export default {
   },
   methods: {
     async registerUserEvent() {
-      const { errors = [] } = await registerUser(this.user);
+      await registerUser(this.user);
     },
   },
 };
