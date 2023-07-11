05b58ced7da75b92281ce3177a390b6f7b23861e
Josroyal <josue.arbulu@utec.edu.pe>
2023-07-11 16:45:14 -0500

    login

---
+++ josroyal_arreglando_login/backend_josroyal/config/local.py
@@ -0,0 +1,5 @@
+config = {
+    'DATABASE_URI': 'postgresql://postgres:josue2003@localhost:5432/skinvue',
+    'SECRET_KEY': 'skinlootdbp20',
+    'ALGORYTHM': 'HS256'
+}
---
+++ josroyal_arreglando_login/backend_josroyal/config/qa.py
@@ -0,0 +1,3 @@
+config = {
+    'DATABASE_URI': 'postgresql://postgres:josue2003@localhost:5432/skltests',
+}
